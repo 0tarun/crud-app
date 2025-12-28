555880f77dc1fd52909a148287041fa5a1d67ae0
0tarun <tdas221498@bscse.uiu.ac.bd>
2025-12-28 23:27:24 +0600

    successfully run client server

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <h1>This is home page!</h1>
+    },
+    {
+      path: "/add",
+      element: <h1>This is add page!</h1>
+    },
+    {
+      path: "/user",
+      element: <h1>This is user page!</h1>
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
