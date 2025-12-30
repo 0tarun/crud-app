294fcde88a689ee3ad1959297e5e95c6b0278c30
0tarun <tdas221498@bscse.uiu.ac.bd>
2025-12-30 13:49:33 +0600

    create add user

---
+++ client/src/components/addUser/Add.jsx
@@ -0,0 +1,13 @@
+import { div } from "framer-motion/client";
+import React from "react";
+const Add = () =>{
+    return(
+        <div className="addUser">
+            <h3>
+                <form>
+
+                </form>
+            </h3>
+        </div>
+    )
+}
